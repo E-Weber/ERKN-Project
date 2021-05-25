8e6248a3e50a4ae4858a12e00b81616a25edc0e1
natalieshaw2020 <nshaw@heartlandcompany.com>
2021-05-25 14:34:09 -0500

    map updates

--- Map/static/js/app.js
+++ Map/static/js/app.js
@@ -725,7 +725,7 @@ var universityMarkers = [];
 
 for (var i = 0; i < universities.length; i++) {
   universityMarkers.push(
-    L.marker(universities[i].Location).bindPopup("<h3>" + universities[i].Rank + ': ' + universities[i].Name + "</h3>").bindTooltip(universities[i].Rank)
+    L.marker(universities[i].Location).bindPopup("<h3>" + universities[i].Rank + ': ' + universities[i].Name + "</h3>").bindTooltip(universities[i].Rank + ': ' + universities[i].Name)
   );
 }
 
